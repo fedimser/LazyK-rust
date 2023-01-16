4fdebe9f6a551857df164e647c6d6e99af87d025
Dmytro Fedoriaka <fedimser@gmail.com>
2023-01-15 22:54:29 -0800

    Use endlines in primes

--- src/parser.rs
+++ src/parser.rs
@@ -616,7 +616,7 @@ mod tests {
         let program = lk.parse(source);
         assert_eq!(
             lk.run_string_limited(program, "", 70),
-            "2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97"
+            "2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97".replace(" ", "\n")
         );
     }
 
